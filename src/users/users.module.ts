14388af6dfa2a7f6431af9f1d06acdac2ea33916
jaiderbernal90 <64909036+jaiderbernal90@users.noreply.github.com>
2024-06-23 16:58:35 -0500

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
